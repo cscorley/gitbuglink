a44c46da2fe8e60a54fddc0dcb47ad5ec7a3d614
Christopher Corley <cscorley@ua.edu>
2012-11-02 18:55:54 -0500

    Switch to Python2 because dulwich hates the future

--- Makefile
+++ Makefile
@@ -7,5 +7,5 @@ test:
 	nosetests tests
 
 init:
-	test -d env || virtualenv env
+	test -d env || virtualenv env --python=python2.7
 	. env/bin/activate; pip install -r requirements.txt --use-mirrors
--- gitbuglink/git.py
+++ gitbuglink/git.py
@@ -6,18 +6,31 @@
 #
 # See LICENSE for details.
 
+from __future__ import (print_function, with_statement)
+
 import re
+import sys
+import os
+import csv
 from collections import namedtuple
 
 import dulwich
 
-TraceInfo = namedtuple('traceinfo', 'commit_id parent_commit_id author committer date message bug_ids')
+TraceInfo = namedtuple('traceinfo', 'commit_id author committer date message bug_ids')
 SuperRe = re.compile("(?:bug|fix|pr|br)\\s*(?:id|[#=])?\\s*([0-9]{4,6})")
-# what about "Bug" "PR" and urls?
+# what about "Bug" "PR", multiple ids, and urls?
 
 def detect(commit):
     ids = list()
+    ids.append(detect_message(commit.message))
+    # filter by time?
     return tuple(ids)
+
+def detect_message(msg):
+    r = SuperRe.match(msg)
+    if r:
+        return r.group(1)
+
 
 def get_links(project_url):
     count = 0
@@ -36,3 +49,15 @@ def get_links(project_url):
             )
 
         yield trace
+
+def main(args):
+    for p in args[1:]:
+        if os.path.exists(p):
+            for each in get_links(args[1]):
+                if each.bug_ids[0] is not None:
+                    print(each.commit_id + "," + each.bug_ids[0])
+        else:
+            print("Path does not exist: ", p)
+
+if __name__ == '__main__':
+    main(sys.argv)
